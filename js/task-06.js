f0804c2e09b9f969581639720c7d8f46b2d8aaa5
AleksandrBreslavets <sasha.bresl2612005@gmail.com>
2023-03-27 13:16:41 +0300

    Update task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,12 @@
 const validationInput = document.querySelector("#validation-input");
 validationInput.addEventListener("blur", oninputValidation)
 function oninputValidation(evt) {
-    if (evt.currentTarget.value.trim().length < Number(validationInput.dataset.length)){
-        validationInput.classList.add("invalid");
+    if (evt.currentTarget.value.trim().length === Number(validationInput.dataset.length)) {
+        validationInput.classList.remove("invalid");
+        validationInput.classList.add("valid");
     }
-    else validationInput.classList.contains("invalid") ? validationInput.classList.replace("invalid", "valid") : validationInput.classList.add("invalid");
+    else {
+        validationInput.classList.add("invalid");
+        validationInput.classList.remove("valid");
+    }
 }
